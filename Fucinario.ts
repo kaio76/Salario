27b5ea8a3b26aa5a5305fa7de8ca195a23d026f5
kaio76 <42898648+kaio76@users.noreply.github.com>
2018-10-08 15:10:40 -0300

    Create Fucinario.ts

---
+++ Fucinario.ts
@@ -0,0 +1,20 @@
+# Salario
+Atividade 03
+export class Funcionario{
+private nome : string;
+private diaria : number;
+public constructor{nome:string, diaria: number;
+
+  this.nome = nome;
+  this. diaria = diaria;
+}
+public getNome(): string{
+    return this.nome;
+}public setNome(nome:string):void{
+    this.nome = nome;
+} public getDiaria(): number{
+    return this.diaria;
+}public setDiaria(diaria:number):void{
+    this.diaria = diaria;
+  }
+}
