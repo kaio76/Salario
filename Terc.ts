7526ab804b767d5be2ffa4fc578fe4ecb8cd4fa4
kaio76 <42898648+kaio76@users.noreply.github.com>
2018-10-23 18:17:17 -0300

    Create Terc.ts

---
+++ Terc.ts
@@ -0,0 +1,35 @@
+import {Funcionario} from "./Funcionario";
+
+export class Terc{
+    
+    private horasTrabalhadas: number;
+
+    public constructor( horasTrabalhadas: number){
+        
+        this.horasTrabalhadas = horasTrabalhadas;
+    }
+    public getHorasTrabalhadas():number{
+      return this.horasTrabalhadas;
+    }public setHorasTrabalhadas(horasTrabalhadas:number):void{
+      this.horasTrabalhadas = horasTrabalhadas;
+    }
+
+  public insalubre():boolean{
+  let insal : number = readline.question("digite  se insalubre\n digite 2 se não for insalubre");
+  if (insal == 2){
+    insal = 0;
+    return false;
+
+  }if(insal == 1){
+    insal = 500;
+    return true;
+  }
+  }public InserirHorasTrab():void{
+    let horasTrabalhadas: number = readline.question("Digite horas trabalhadas");
+    }public calcSalario(insal:number, ):number{
+      let salario:number = 4 * this.horasTrabalhadas + insal ;
+      return salario; 
+    }
+
+  }
+  
