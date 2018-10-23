52e1d7eb9eb00ed74765185067a0b31d2951e971
kaio76 <42898648+kaio76@users.noreply.github.com>
2018-10-23 18:16:36 -0300

    Update Sta.ts

--- Sta.ts
+++ Sta.ts
@@ -21,7 +21,16 @@ export class Sta{
     let nv : number = readline.question("Digite o nivel");
       this.nivel = nv;
       return true;
-  }public inserirDiaria(): bolean{
-    if
+  }public iserirDiaria(diaria : number):boolean{
+      if(this.diaria > 1){
+        return false;
+      }else{
+        this.diaria = this.diaria + 1;
+        return true;
+      }
+    }public calcSalario(nv : number,diaria:number):number{
+      let salario:number = 3000 + 300 * nv + 100 * diaria;
+      return salario; 
+    }
+
   }
-}
