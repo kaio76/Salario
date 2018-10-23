21e4b194334736bafb39e57ab9cafa489cbfdb04
kaio76 <42898648+kaio76@users.noreply.github.com>
2018-10-23 19:09:09 -0300

    Update Sta.ts

--- Sta.ts
+++ Sta.ts
@@ -1,24 +1,20 @@
 import {Funcionario} from "./Funcionario";
 export class Sta{
-  private diaria : number;
+  
   private nivel : number;
 
-  public constructor(diaria:number, nivel:number){
-    this.diaria = diaria;
+  public constructor( nivel:number){
+    super(nome, diaria);
     this.nivel = nivel;
   }
-  public getDiaria():number{
-    return this.diaria;
-  }public setDiaria(diaria:number):void{
-    this.diaria = diaria;
-  }public getNivel():number{
+  public getNivel():number{
     return this.nivel;
   }public setNivel(nivel:number):void{
     this.nivel = nivel;
   }
   //metodos
   public inserirNivel(nivel:number):boolean{
-    let nv : number = readline.question("Digite o nivel");
+    
       this.nivel = nv;
       return true;
   }public iserirDiaria(diaria : number):boolean{
